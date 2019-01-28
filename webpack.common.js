547f0e70380343c79d1cdf47c3a26cb1fba2b19e
joel-ortiz <joel.ortiz@schibsted.com>
2019-01-28 12:53:34 +0100

    Update webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -62,7 +62,7 @@ module.exports = {
           {
             loader: 'file-loader',
             options: {
-              name: './assets/[hash].[ext]'
+              name: 'assets/[hash].[ext]'
             }
           }
         ]
